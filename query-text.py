c54751ace26dd107551003a06ead1ce6e5aca255
eriktks <erikt@xs4all.nl>
2020-05-18 09:52:54 +0200

    initial version

---
+++ query-text.py
@@ -0,0 +1,40 @@
+#!/usr/local/bin/python3
+# query.py: run a fixed query on a tweet file
+# usage: query.py
+# 20200410 erikt(at)xs4all.nl
+
+import csv
+import json
+import re
+import sys
+
+NL = "nl"
+TEXT = "text"
+LANG = "lang"
+IDSTR = "id_str"
+FULLTEXT = "full_text"
+EXTENDEDTWEET = "extended_tweet"
+RETWEETEDSTATUS = "retweeted_status"
+
+def replaceNewlines(text):
+    return(re.sub(r"\n",r"\\n",text))
+
+csvwriter = csv.DictWriter(sys.stdout,[IDSTR,TEXT])
+csvwriter.writeheader()
+for line in sys.stdin:
+    #try:
+    if True:
+        jsonLine = json.loads(line)
+        lang = jsonLine[LANG]
+        if lang == NL:
+            idstr = jsonLine[IDSTR]
+            text = jsonLine[TEXT]
+            if EXTENDEDTWEET in jsonLine and \
+               FULLTEXT in jsonLine[EXTENDEDTWEET]:
+                text = jsonLine[EXTENDEDTWEET][FULLTEXT]
+            if RETWEETEDSTATUS in jsonLine and \
+               EXTENDEDTWEET in jsonLine[RETWEETEDSTATUS] and \
+               FULLTEXT in jsonLine[RETWEETEDSTATUS][EXTENDEDTWEET]:
+                text = jsonLine[RETWEETEDSTATUS][EXTENDEDTWEET][FULLTEXT]
+            csvwriter.writerow({IDSTR:idstr,TEXT:replaceNewlines(text)})
+    #except: pass
